a760a4907fd48ae3ab7a8500463eefc538ebe3c3
Anas ESSAMLALI <anasessamlali1@gmail.com>
2024-01-22 14:04:04 +0100

    First Push of the Activity 6

---
+++ src/main/java/org/sid/demo2springcloudstreamskafka/Demo2SpringcloudStreamsKafkaApplication.java
@@ -0,0 +1,13 @@
+package org.sid.demo2springcloudstreamskafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2SpringcloudStreamsKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2SpringcloudStreamsKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sid/demo2springcloudstreamskafka/Demo2SpringcloudStreamsKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.demo2springcloudstreamskafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2SpringcloudStreamsKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
